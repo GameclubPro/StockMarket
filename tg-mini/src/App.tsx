fb7079274311b5d6fc2b627a15337a7b94bbeeef
root <root@DESKTOP-R3QOEAF.localdomain>
2026-02-06 01:30:01 +0300

    deploy: update 2026-02-06-0130

--- tg-mini/src/App.tsx
+++ tg-mini/src/App.tsx
@@ -564,7 +564,6 @@ export default function App() {
       if (typeof data.balance === 'number') {
         setPoints(data.balance);
       }
-      setTaskLink('');
       setReactionLink('');
       setSelectedGroupId('');
       setSelectedGroupTitle('');
